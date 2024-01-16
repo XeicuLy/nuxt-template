c93a034c09f6a3414b1fbde0a20f9e940cf57608
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-16 20:57:41 +0900

    🔧 : TailwindCSSの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  srcDir: '/src',
+  ssr: true,
+  css: ['@/styles/globals.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  },
 });
