8523cebd7756dcb57ba5c973b9caa568358b868c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-22 19:49:15 +0900

    💄 : サンプルコンポーネント実装

---
+++ src/components/Sample.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+const msg = ref('Hello World!');
+</script>
+<template>
+  <div>{{ msg }}</div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from '~/components/ui/Button.vue';
+</script>
+<template>
+  <section>
+    <div>こんにちは</div>
+    <Button>ボタン</Button>
+    <Sample />
+  </section>
+</template>
